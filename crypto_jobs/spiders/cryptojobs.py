9ea9e14c9ca35ca3f4f428ed210d04087aa66108
hassan ali <hassanali5120@gmail.com>
2021-02-25 22:17:46 +0500

    next page url call needed parse not working well.

--- crypto_jobs/spiders/cryptojobs.py
+++ crypto_jobs/spiders/cryptojobs.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import w3lib.html
 
 class CryptojobsSpider(scrapy.Spider):
     name = 'cryptojobs'
@@ -7,5 +7,28 @@ class CryptojobsSpider(scrapy.Spider):
     start_urls = ['https://crypto.jobs/']
 
     def parse(self, response):
+
+        jobs=response.css('.table-jobs').xpath('//tbody/tr[@class="highlighted"]')
+        for job in jobs:
+            # print(job)
+            # company_ico=job.css('img::attr(src)').get()
+            job_url=job.css('.job-url::attr(href)').get()
+            # company_name=job.css('.job-url span::text').get()
+            # job_type_container=job.css('.hidden-xs').xpath('//small/span[1]').get()
+            # job_type = w3lib.html.remove_tags(job_type_container)
+            # job_industry_container = job.css('.hidden-xs').xpath('//small/span[2]').get()
+            # job_industry = w3lib.html.remove_tags(job_type_container)
+            url = response.urljoin(job_url)
+            if url:
+                yield scrapy.Request(url=url, callback=self.single_parse)
+
+        nextUrl=response.css('ul.pagination li:last-of-type a::attr(href)').get()
+        if nextUrl:
+            nextPageUrl=response.urljoin(nextUrl)
+            yield scrapy.Request(url=nextPageUrl, callback=self.parse)
+
+
+
+    def single_parse(self,response):
+        items=dict()
         print(response)
-        print(1)
